736406f0a7a2844039a7748b692585dd41210eec
Angel De La Cruz <angelfranciscoprivado@gmail.com>
2023-02-06 09:51:44 -0500

    made the chapter cleaner script watch for changes

--- bookChapterCleaner.js
+++ bookChapterCleaner.js
@@ -2,6 +2,28 @@ const elements = document.querySelectorAll(".v-card__subtitle.pa-2.pb-1.text--pr
 const regex = /Chapter\s(\w+)/;
 
 for (const element of elements) {
+  modifyElement(element);
+}
+
+const observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    Array.from(mutation.addedNodes).forEach(node => {
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        const elements = node.querySelectorAll(".v-card__subtitle.pa-2.pb-1.text--primary");
+        for (const element of elements) {
+          modifyElement(element);
+        }
+      }
+    });
+  });
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
+
+function modifyElement(element) {
   const text = element.innerHTML;
   const match = text.match(regex);
   if (match) {
